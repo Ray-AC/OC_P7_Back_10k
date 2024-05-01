6250431e86e4ca0d1aa6a07ac755de742f04feb8
Ray AC <ac.ray2000@gmail.com>
2024-05-01 10:44:54 +0200

    pytest provisoire

--- tests/tests_prediction.py
+++ tests/tests_prediction.py
@@ -2,9 +2,15 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from ..tab.prediction import tab_prediction_client
-from ..tab.prediction_live import tab_prediction_client_live
+import pytest
+from tab.prediction import tab_prediction_client
+from tab.prediction_live import tab_prediction_client_live
 
-def test_prediction_client():
-    assert tab_prediction_client(100002) == 1.0
-    assert tab_prediction_client_live(100246) == 1.0
+@pytest.mark.asyncio
+async def test_prediction_client():
+    test_pred = await tab_prediction_client(100002)
+    assert test_pred['oof_preds'] == 1.0
+    test_pred_live = await tab_prediction_client_live(100246)
+    assert test_pred_live['Pourcentage de chance de remboursement'] == 0.59
+'''    assert await tab_prediction_client(100002) == 1.0
+    assert await tab_prediction_client_live(100246) == 1.0'''
