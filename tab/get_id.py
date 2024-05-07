e49b9f88a3cecf78688051d0d4d5845cbd722bb1
Ray AC <ac.ray2000@gmail.com>
2024-05-07 10:36:33 +0200

    test d'optimisation de code v1 (dfs+requirements)

--- tab/get_id.py
+++ tab/get_id.py
@@ -1,7 +1,5 @@
 from imports import *
 
-predict_df = pd.read_csv("./data/predict_10k_rows.csv")
-
-async def tab_get_unique_client_ids():
+async def tab_get_unique_client_ids(predict_df):
     unique_client_ids = predict_df['sk-id-curr'].unique().tolist()
     return unique_client_ids
